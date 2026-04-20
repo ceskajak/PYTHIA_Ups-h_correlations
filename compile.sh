668e54bcf947403262878500f0df04e43f1d231a
Jakub Ceska <ceskajak@fjfi.cvut.cz>
2026-04-20 12:19:17 +0200

    changed compile.sh to choose which files to compile

--- compile.sh
+++ compile.sh
@@ -6,4 +6,4 @@ else
     FILES=./*.cc
 fi
 
-g++ ./*.cc -o RunPythia `pythia8-config --cxxflags --ldflags` `root-config --cflags --glibs` -lGenVector
+g++ $FILES -o RunPythia `pythia8-config --cxxflags --ldflags` `root-config --cflags --glibs` -lGenVector
